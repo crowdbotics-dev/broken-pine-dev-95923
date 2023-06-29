4390f852817711f569bd9518de5e4322cf9ced37
Crowdbotics <engineering@crowdbotics.com>
2023-06-29 10:42:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-pine-dev-95923",
+  url: "https://broken-pine-dev-95923.botics.co",
+  api: "https://broken-pine-dev-95923.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
